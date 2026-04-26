60aaa5d23e07b02fc741717c0f295d99f0020f63
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-04-26 17:08:16 +0100

    Create defaultPlayers.js

---
+++ defaultPlayers.js
@@ -0,0 +1,15 @@
+// Default pre‑made characters for the cast picker.
+// You can expand this list infinitely.
+
+window.DEFAULT_PLAYERS = [
+  { name: "Ava Storm" },
+  { name: "Leo Hart" },
+  { name: "Mira Vale" },
+  { name: "Jasper Quinn" },
+  { name: "Nova Reed" },
+  { name: "Kai Rivers" },
+  { name: "Sage Monroe" },
+  { name: "Ember Fox" },
+  { name: "Rowan Blake" },
+  { name: "Luna Frost" }
+];
